6fa3fcf07a1739e5c7f5ee0806e69003c2ad5887
davidPerucho <david.rbdh@gmail.com>
2024-12-18 21:20:09 +0100

    Listado de imágenes terminado.

--- Mini Pueblo/Assets/Scripts/ImageGenarator/ShowImages.cs
+++ Mini Pueblo/Assets/Scripts/ImageGenarator/ShowImages.cs
@@ -6,18 +6,54 @@ using UnityEngine.UI;
 
 public class ShowImages : MonoBehaviour
 {
+    const int MAX_IMAGES = 3;
+    const float BUTTONS_TRANSPARENCY = 0.4f;
+
     [SerializeField]
     Transform imageLayout; //Un GameObject con un Horizontal Layout Group para posicionar las imágenes
 
     [SerializeField]
     GameObject imagePrefab; //Un prefab con un componente Image
 
+    [SerializeField]
+    Button leftButon;
+
+    [SerializeField]
+    Button rightButton;
+
     string imagesDirectory;
+    int leftIndex = 0;
+    int rightIndex = 0;
+    int numImages = MAX_IMAGES;
+    string[] imageFiles;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Añado a cada boton su función correspondiente
+        leftButon.onClick.RemoveAllListeners();
+        leftButon.onClick.AddListener(moveLeft);
+
+        rightButton.onClick.RemoveAllListeners();
+        rightButton.onClick.AddListener(moveRight);
+
+        //Ruta al directorio donde se guardan las imágenes
         imagesDirectory = Path.Combine(Application.persistentDataPath, "GeneratedImages");
+
+        //Obtener todos los archivos PNG en el directorio
+        imageFiles = Directory.GetFiles(imagesDirectory, "*.png");
+
+        numImages = imageFiles.Length;
+
+        //Se muestran como mucho tres imágenes a la vez
+        if (numImages > 3 )
+        {
+            rightIndex = 2;
+        }
+        else
+        {
+            rightIndex = numImages - 1;
+        }
 
         //Comprobar que el directorio existe
         if (Directory.Exists(imagesDirectory))
@@ -33,47 +69,90 @@ public class ShowImages : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Image imageButtonLeft = leftButon.GetComponent<Image>();
+        Image imageButtonRight = rightButton.GetComponent<Image>();
+        Color colorButtonLeft = imageButtonLeft.color;
+        Color colorButtonRight = imageButtonRight.color;
+
+        if (leftIndex == 0 || numImages <= 3)
+        {
+            leftButon.enabled = false;
+            colorButtonLeft.a = Mathf.Clamp01(BUTTONS_TRANSPARENCY);
+            imageButtonLeft.color = colorButtonLeft;
+        }
+        else
+        {
+            leftButon.enabled = true;
+            colorButtonLeft.a = Mathf.Clamp01(1);
+            imageButtonLeft.color = colorButtonLeft;
+        }
+
+        if (rightIndex == (numImages-1) || numImages <= 3)
+        {
+            rightButton.enabled = false;
+            colorButtonRight.a = Mathf.Clamp01(BUTTONS_TRANSPARENCY);
+            imageButtonRight.color = colorButtonRight;
+        }
+        else
+        {
+            rightButton.enabled = true;
+            colorButtonRight.a = Mathf.Clamp01(1);
+            imageButtonRight.color = colorButtonRight;
+        }
     }
 
     void DisplayImages()
     {
-        int numImages = 3;
-
-        // Obtener todos los archivos PNG en el directorio
-        string[] imageFiles = Directory.GetFiles(imagesDirectory, "*.png");
-
-        //Como maximo se muestran tres imágenes en pantalla
-        if (imageFiles.Length < 3)
+        //Eliminar todas las imagenes que hay en la lista actualmente
+        foreach (Transform child in imageLayout)
         {
-            numImages = imageFiles.Length;
+            Destroy(child.gameObject);
         }
 
         // Cargar las tres primeras imágenes y mostrarlas
-        for (int i = 0; i < numImages; i++)
+        if (numImages > 0)
         {
-            string filePath = imageFiles[i];
-
-            // Cargar la imagen desde el archivo
-            byte[] imageBytes = File.ReadAllBytes(filePath);
-            Texture2D texture = new Texture2D(2, 2);
-            texture.LoadImage(imageBytes);
-
-            // Crear un nuevo Sprite a partir de la textura
-            Sprite sprite = Sprite.Create(
-                texture,
-                new Rect(0, 0, texture.width, texture.height),
-                new Vector2(0.5f, 0.5f)
-            );
-            
-            // Instanciar el prefab y configurarlo
-            GameObject imageObject = Instantiate(imagePrefab, imageLayout);
-            Image uiImage = imageObject.GetComponent<Image>();
-            if (uiImage != null)
+            for (int i = leftIndex; i <= rightIndex; i++)
             {
-                uiImage.sprite = sprite;
-                uiImage.preserveAspect = true;
+                string filePath = imageFiles[i];
+
+                // Cargar la imagen desde el archivo
+                byte[] imageBytes = File.ReadAllBytes(filePath);
+                Texture2D texture = new Texture2D(2, 2);
+                texture.LoadImage(imageBytes);
+
+                // Crear un nuevo Sprite a partir de la textura
+                Sprite sprite = Sprite.Create(
+                    texture,
+                    new Rect(0, 0, texture.width, texture.height),
+                    new Vector2(0.5f, 0.5f)
+                );
+
+                // Instanciar el prefab y configurarlo
+                GameObject imageObject = Instantiate(imagePrefab, imageLayout);
+                Image uiImage = imageObject.GetComponent<Image>();
+                if (uiImage != null)
+                {
+                    uiImage.sprite = sprite;
+                    uiImage.preserveAspect = true;
+                }
             }
         }
+    }
+
+    void moveRight()
+    {
+        leftIndex++;
+        rightIndex++;
+
+        DisplayImages();
+    }
+
+    void moveLeft()
+    {
+        leftIndex--;
+        rightIndex--;
+
+        DisplayImages();
     }
 }
