9d95e50ef21441c0189b9322264d070456e1ba63
davidPerucho <david.rbdh@gmail.com>
2025-03-03 21:09:44 +0100

    Terminada pantalla de selección de personaje para la creación de escenas.

--- Mini Pueblo/Assets/Scripts/UI/MainMenu.cs
+++ Mini Pueblo/Assets/Scripts/UI/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 
@@ -12,12 +13,19 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     FileDataManager fileDataManager; //Manejador de datos guardados del juego
+    RawImage[] characters; //Almacena las imágenes de los personajes
+    int screenindex = 0; //Index que indica la parte del menu que se está mostrando
+    int characterIndex = 0;
+
+    [SerializeField]
+    GameObject characterSelection; //Selector de personajes
 
     // Start is called before the first frame update
     void Start()
     {
         UIManager.Instance.AddListenerToButton("Jugar", play);
-        UIManager.Instance.AddListenerToButton("Crear", create);
+        UIManager.Instance.AddListenerToButton("Continuar", continueCreation);
+        UIManager.Instance.AddListenerToButton("Crear", continueCreation);
         UIManager.Instance.AddListenerToButton("Volver", returnToMenu);
         UIManager.Instance.AddListenerToButton("CrearMenu", openCreateSceneinterface);
         fileDataManager = new FileDataManager(Application.persistentDataPath, "datos.json");
@@ -54,46 +62,113 @@ public class MainMenu : MonoBehaviour
     {
         UIManager.Instance.DisableObject("Nueva");
         UIManager.Instance.DisableObject("Jugar");
-        UIManager.Instance.DisableObject("TextoMenu");
         UIManager.Instance.DisableObject("CrearMenu");
         UIManager.Instance.DisableObject("Opciones");
 
-        UIManager.Instance.EnableObject("Crear");
+        UIManager.Instance.EnableObject("Continuar");
         UIManager.Instance.EnableObject("Volver");
         UIManager.Instance.EnableObject("InputNombre");
         UIManager.Instance.EnableObject("TextoCrear");
+
+        UIManager.Instance.SetText("TextoMenu", "CREAR MINIJUEGO");
     }
 
     /// <summary>
     /// Carga la escena de cración de minijuegos/actividades.
     /// </summary>
-    void create()
+    void continueCreation()
     {
-        string inputText = UIManager.Instance.GetInputValue("InputNombre");
+        if (screenindex == 0) //Insercción del nombre
+        {
+            string inputText = UIManager.Instance.GetInputValue("InputNombre");
 
-        if (inputText == "") //En caso de que el jugador no introduzca ningún texto muestro un error
-        {
-            UIManager.Instance.SetText("Error", "Tienes que ponerle un nombre al juego");
-            UIManager.Instance.EnableObject("Error");
+            if (inputText == "") //En caso de que el jugador no introduzca ningún texto muestro un error
+            {
+                UIManager.Instance.SetText("Error", "Tienes que ponerle un nombre al juego");
+                UIManager.Instance.EnableObject("Error");
+            }
+            else if (sceneExists(inputText) == true) //En caso de que ya exista una escena con el nombre introducido muestro un error
+            {
+                UIManager.Instance.SetText("Error", "El nombre no es válido");
+                UIManager.Instance.EnableObject("Error");
+            }
+            else if (jsonExists(inputText) == false)
+            {
+                //Guardo el nombre de la escena y paso a la selección de personaje
+                PlayerPrefs.SetString("SceneName", inputText);
+                PlayerPrefs.Save();
+                startCharacterSelection();
+            }
+            else
+            {
+                //Guardo el nombre de la escena para su posterior creación y cargo la escena de creación de minijuegos/actividades
+                PlayerPrefs.SetString("SceneName", inputText);
+                PlayerPrefs.Save();
+                SceneManager.LoadScene("SceneCreation");
+            }
         }
-        else if (sceneExists(inputText) == true) //En caso de que ya exista una escena con el nombre introducido muestro un error
+        else if (screenindex == 1) //Selección del personaje
         {
-            UIManager.Instance.SetText("Error", "Ya existe un minijuego con este nombre");
-            UIManager.Instance.EnableObject("Error");
+            if (characterIndex == 0)
+            {
+                UIManager.Instance.SetText("Error", "Tienes que seleccionar un personaje");
+                UIManager.Instance.EnableObject("Error");
+            }
+            else
+            {
+                Debug.Log("Se ha seleccionado la imagen: " + characterIndex);
+                PlayerPrefs.SetString("SelectedNPC", characterIndex.ToString());
+                PlayerPrefs.Save();
+            }
         }
-        else
+        else //Selección de la localización
         {
-            //Guardo el nombre de la escena para su posterior creación y cargo la escena de creación de minijuegos/actividades
-            PlayerPrefs.SetString("SceneName", inputText);
-            PlayerPrefs.Save();
-            SceneManager.LoadScene("SceneCreation");
+
         }
+    }
+
+    /// <summary>
+    /// Inicializa los elementos de la selección de personaje.
+    /// </summary>
+    void startCharacterSelection()
+    {
+        UIManager.Instance.SetText("TextoMenu", "SELECCIONAR PERSONAJE");
+        UIManager.Instance.DisableObject("InputNombre");
+        characterSelection.SetActive(true);
+
+        //Obtengo las imágenes de los personajes
+        characters = characterSelection.GetComponentsInChildren<RawImage>();
+
+        foreach (RawImage character in characters)
+        {
+            character.color = new Color(character.color.r, character.color.g, character.color.b, 0.5f);
+            GameObject gameObject = character.gameObject;
+            if (gameObject.GetComponent<Button>() == null)
+            {
+                Button button = gameObject.AddComponent<Button>();
+                button.onClick.AddListener(() => characterSelected(gameObject));
+            }
+        }
+
+        screenindex++;
+    }
+
+    /// <summary>
+    /// Inicializa los elementos de la selección de localización.
+    /// </summary>
+    void startLocationSelection()
+    {
+        UIManager.Instance.SetText("TextoMenu", "SELECCIONAR LOCALIZACIÓN");
+        characterSelection.SetActive(false);
+
+        screenindex++;
     }
 
     /// <summary>
     /// Devuelve true si ya existe una escena con el nombre pasado por argumento.
     /// </summary>
     /// <param name="sceneName">Nombre de la escena que se quiere crear.</param>
+    /// <returns>True si existe la escena false si no</returns>
     bool sceneExists(string sceneName)
     {
         string scenesPath = Path.Combine(Application.dataPath, "Scenes/");
@@ -111,19 +186,45 @@ public class MainMenu : MonoBehaviour
     }
 
     /// <summary>
+    /// Devuelve true si ya existe un json de una escena con el nombre pasado por argumento.
+    /// </summary>
+    /// <param name="sceneName">Nombre de la escena que se quiere crear.</param>
+    /// <returns>True si existe el json de la escena false si no</returns>
+    bool jsonExists(string sceneName)
+    {
+        string scenesPath = Path.Combine(Application.persistentDataPath, "Scenes/");
+        if (!Directory.Exists(scenesPath))
+        {
+            Directory.CreateDirectory(scenesPath);
+            Debug.Log($"El directorio no existía, pero se ha creado: {scenesPath}");
+        }
+
+        string[] sceneFiles = Directory.GetFiles(scenesPath, "*.json", SearchOption.AllDirectories);
+
+        foreach (string scene in sceneFiles)
+        {
+            if (Path.GetFileNameWithoutExtension(scene) == sceneName)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
     /// Activa los elementos de UI del menu y desactiva los de la creación de minijuegos/actividades.
     /// </summary>
     void returnToMenu()
     {
         UIManager.Instance.DisableObject("Crear");
+        UIManager.Instance.DisableObject("Continuar");
         UIManager.Instance.DisableObject("Volver");
         UIManager.Instance.DisableObject("InputNombre");
-        UIManager.Instance.DisableObject("TextoCrear");
         UIManager.Instance.DisableObject("Error");
 
         UIManager.Instance.EnableObject("Nueva");
         UIManager.Instance.EnableObject("Jugar");
-        UIManager.Instance.EnableObject("TextoMenu");
         UIManager.Instance.EnableObject("CrearMenu");
         UIManager.Instance.EnableObject("Opciones");
 
@@ -131,6 +232,15 @@ public class MainMenu : MonoBehaviour
         {
             UIManager.Instance.DisableObject("Jugar");
         }
+
+        characterSelection.gameObject.SetActive(false);
+        if (characterIndex != 0)
+        {
+            characters[characterIndex - 1].color = new Color(characters[characterIndex - 1].color.r, characters[characterIndex - 1].color.g, characters[characterIndex - 1].color.b, 0.5f);
+        }
+        characterIndex = 0;
+        screenindex = 0;
+        UIManager.Instance.SetText("TextoMenu", "MINI PUEBLO");
     }
 
     /// <summary>
@@ -142,5 +252,20 @@ public class MainMenu : MonoBehaviour
         UIManager.Instance.DisableButton("Jugar");
         UIManager.Instance.DisableButton("Opciones");
         UIManager.Instance.DisableObject("CrearMenu");
+    }
+
+    /// <summary>
+    /// Elimina la selección anterior de personaje y realiza la nueva.
+    /// </summary>
+    /// <param name="gameObject">Objeto sobre el que se ha hecho click.</param>
+    void characterSelected(GameObject gameObject)
+    {
+        int imageIndex = characterIndex - 1;
+        if (characterIndex != 0) 
+            characters[imageIndex].color = new Color(characters[imageIndex].color.r, characters[imageIndex].color.g, characters[imageIndex].color.b, 0.5f);
+
+        imageIndex = int.Parse(gameObject.name) - 1;
+        characters[imageIndex].color = new Color(characters[imageIndex].color.r, characters[imageIndex].color.g, characters[imageIndex].color.b, 1);
+        characterIndex = imageIndex + 1;
     }
 }
