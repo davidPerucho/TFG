dd6da8fd6cf22be9e333331e8d02d8acc990e999
davidPerucho <david.rbdh@gmail.com>
2025-03-07 21:10:14 +0100

    Añadido guardado inicial de escena.

--- Mini Pueblo/Assets/Scripts/SceneCreation/CreationManager.cs
+++ Mini Pueblo/Assets/Scripts/SceneCreation/CreationManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -16,13 +17,10 @@ public class CreationManager : MonoBehaviour
 
     void Awake()
     {
-        //Agrego funciones de UI
-        UIManager.Instance.AddListenerToButton("returnButton", () => { yesNoUI.SetActive(true); });
-        UIManager.Instance.AddListenerToButton("No", () => { SceneManager.LoadScene("MainMenu"); });
-
         //Creo la nueva escena con el nombre insertado en el menu de creación
         string sceneName = PlayerPrefs.GetString("SceneName", "Minijuego");
         createdScene = SceneManager.CreateScene(sceneName);
+        addCamera(); //Añado una cámara a la escena
 
         //Creo la ruta de guardado para la escena
         if (!Directory.Exists(Path.Combine(Application.persistentDataPath, "Scenes/"))) //Comprobar que el directorio existe
@@ -38,9 +36,54 @@ public class CreationManager : MonoBehaviour
         Debug.Log("Nombre escena: " + sceneName + ", Index personaje: " + characterIndex + ", Index localización: " + locationIndex);
     }
 
+    void Start()
+    {
+        //Agrego funciones de UI
+        UIManager.Instance.AddListenerToButton("returnButton", () => { yesNoUI.SetActive(true); });
+        UIManager.Instance.AddListenerToButton("No", () => { SceneManager.LoadScene("MainMenu"); });
+        UIManager.Instance.AddListenerToButton("Si", saveScene);
+    }
+
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void saveScene()
+    {
+        try
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(sceneSavePath));
+            string dataToStore = JsonUtility.ToJson(createdScene, true);
+
+            using (FileStream stream = new FileStream(sceneSavePath, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(dataToStore);
+                }
+            }
+
+            SceneManager.LoadScene("MainMenu");
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+    void addCamera()
+    {
+        //Creo la cámara
+        GameObject cameraObject = new GameObject("Camera");
+        Camera cameraComponent = cameraObject.AddComponent<Camera>();
+
+        cameraComponent.clearFlags = CameraClearFlags.Skybox; //Configuro la vista
+        cameraComponent.backgroundColor = Color.blue; //Configuro el color de fondo
+        cameraComponent.orthographic = false;
+        cameraObject.tag = "MainCamera"; //Añado un tag para que Unity la reconozca como la cámara principal
+
+        SceneManager.MoveGameObjectToScene(cameraObject, createdScene); //Añado la cámara a la escena
     }
 }
