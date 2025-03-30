293a8097ea035d6dc25bac3ce5d1b815cb990ad5
davidPerucho <david.rbdh@gmail.com>
2025-03-30 20:21:15 +0200

    Añadido menu de pausa.

---
+++ Mini Pueblo/Assets/pause_controller.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseController : MonoBehaviour
+{
+    [SerializeField]
+    Button pauseButton; //Botón de pausa
+
+    [SerializeField]
+    GameObject pauseUI; //Objeto con la interfaz del menu de pausa
+
+    [SerializeField]
+    Button resume; //Botón para reanudar el juego
+
+    [SerializeField]
+    Button mainMenu; //Botón para volver al menu principal
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Añado las funciones a los botones
+        pauseButton.onClick.AddListener(pauseGame);
+        mainMenu.onClick.AddListener(() => { DataPersitence.instance.saveGame(); SceneManager.LoadScene("MainMenu"); });
+        resume.onClick.AddListener(resumeGame);
+    }
+
+    /// <summary>
+    /// Pausa el juego y muestra el menu de pausa.
+    /// </summary>
+    void pauseGame()
+    {
+        Time.timeScale = 0;
+        pauseUI.SetActive(true);
+    }
+
+    /// <summary>
+    /// Reanuda el juego.
+    /// </summary>
+    void resumeGame()
+    {
+        Time.timeScale = 1;
+        pauseUI.SetActive(false);
+    }
+}
